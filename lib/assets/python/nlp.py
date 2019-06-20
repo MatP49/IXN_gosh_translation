c95305aaab9308a6410ca69f2a62475b15fd90df
matthis <matthis.peltier@gmail.com>
2019-06-20 13:55:39 +0100

    make it better

--- lib/assets/python/nlp.py
+++ lib/assets/python/nlp.py
@@ -4,22 +4,17 @@ import nltk
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 from textblob import TextBlob
-print("ok")
+
 
 text = open("transcript.txt")
-print("ok")
 b=text.read()
-print(b)
 
 
-print("ok")
 
 
 
 tokens = TextBlob(b)
 a= tokens.tags
-print(a)
-#print(tagged)
 with open('lib/assets/python/medical.txt') as file:
     medical_list = [i.strip() for i in file]
 
@@ -27,23 +22,25 @@ with open('lib/assets/python/medical.txt') as file:
 
 filtered = [t for t in a if t[1] == "NN" or t[1] == "JJ"]
 
-print(filtered)
 
 new=[]
 for i in range(len(filtered)):
     new.append(filtered[i][0])
 
 
-print(new)
 
 final=[i for i in new if i in medical_list]
 final=list(set(final))
 
-print(final) 
 
 from textblob import Word
 
 print("ok")
-
+text = open("transcript.txt")
 for i in final:
 	print(Word(i).definitions)
+	f.write(Word(i).definitions)
+
+
+print("ok")
+f.close()
