e99cc8bcfa9590f57a3fae45a1a66530ae12bfe0
matthis <matthis.peltier@gmail.com>
2019-06-20 12:44:09 +0100

    make it better

--- lib/assets/python/nlp.py
+++ lib/assets/python/nlp.py
@@ -8,22 +8,17 @@ print("ok")
 b=text.read()
 print(b)
 
-import spacy
 
-nlp = spacy.load('en_core_web_sm')
-doc = nlp(u'Apple is looking at buying U.K. startup for $1 billion')
-
-for token in doc:
-    print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
-            token.shape_, token.is_alpha, token.is_stop)
+from textblob import TextBlob
+print("ok")
 
 
-tokens = TextBlob(text)
-print(tokens)
-print(blob.tags) 
-#tokens = nltk.word_tokenize(b)
-tagged = nltk.pos_tag(tokens)
-print(tokens)
+b="fewfwef  fwef    fwefew"
+
+
+tokens = TextBlob(b)
+a= tokens.tags
+print(a)
 #print(tagged)
 with open('lib/assets/python/medical.txt') as file:
     medical_list = [i.strip() for i in file]
