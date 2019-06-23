e6d17753092cef6515e89ed45f32b85938077663
matthis <matthis.peltier@gmail.com>
2019-06-23 14:23:40 +0100

    make it better

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,12 +74,11 @@ config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
 address:              'smtp.gmail.com',
 port:                 587,
-domain:               'https://babeluclx.herokuapp.com/',
+domain:               'gmail.com',
 user_name:            'testbabelucl@gmail.com',
 password:             'sfnnmujufpnwsldd',
 authentication:       'plain',
 enable_starttls_auto: true  
-smtp_openssl_verify_mode: ‘none’
 
 
 
